ebf6d1e14f75821222a8de49184ade55442615da
skipty1 <95907033+skipty1@users.noreply.github.com>
2021-12-10 14:25:20 +0600

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -430,16 +430,16 @@ class TitleState extends MusicBeatState
 			case 12:
 				deleteCoolText();
 			// credTextShit.visible = false;
-			// credTextShit.text = "Friday";
+			// credTextShit.text = "Night";
 			// credTextShit.screenCenter();
 			case 13:
-				addMoreText('Friday');
+				addMoreText('Night');
 			// credTextShit.visible = true;
 			case 14:
-				addMoreText('Night');
-			// credTextShit.text += '\nNight';
+				addMoreText('Madness');
+			// credTextShit.text += '\nMadness';
 			case 15:
-				addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
+				addMoreText('Party'); // credTextShit.text += '\nParty';
 
 			case 16:
 				skipIntro();
